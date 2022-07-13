904960d1bd657e387aa56702abeef219ee9a220d
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-07-13 15:29:38 -0400

    added. post-blog route

--- routes/blogs.js
+++ routes/blogs.js
@@ -12,11 +12,11 @@ router.get("/all-blogs", async (req, res, next) => {
   const sortField = req.query.sortField;
   const sortOrder = req.query.sortOrder;
   const filterField = req.query.filterField;
-  const filterValue = req.query.filterValue.toLowerCase();
+  const filterValue = req.query.filterValue;
 
   try {
     const collection = await blogsDB().collection("posts");
-    // const allBlogs = await collection.find({}).toArray();
+    // // const allBlogs = await collection.find({}).toArray();
     let filterObj = {};
     if (filterField && filterValue) {
       filterObj = { [filterField]: filterValue };
@@ -26,7 +26,7 @@ router.get("/all-blogs", async (req, res, next) => {
       let order = (sortOrder === "ASC") ? 1 : -1;
       sortObj = { [sortField]: order };
     }
-    // console.log(filterObj);
+    console.log(filterObj);
     const dbResult = await collection
       .find(filterObj)
       .sort(sortObj)
@@ -34,10 +34,52 @@ router.get("/all-blogs", async (req, res, next) => {
       .skip(skip)
       .toArray();
     //sends response
-    console.log(dbResult);
+    // console.log(dbResult);
     res.json(dbResult);
   } catch (e) {
     console.log(e);
+  }
+});
+
+// POST BLOGS
+
+router.post("/blog-submit", async (req, res) => {
+  try {
+    const blogIsValid = serverCheckBlogIsValid(req.body);
+
+    if (!blogIsValid) {
+      res.status(400).json({
+        message:
+          "To submit a blog you must include Title, Author, Category, and Text.",
+        success: false,
+      });
+      return;
+    }
+
+    const collection = await blogsDB().collection("posts50");
+    const sortedBlogArr = await collection.find({}).sort({ id: 1 }).toArray();
+    const lastBlog = sortedBlogArr[sortedBlogArr.length - 1];
+    const title = req.body.title;
+    const text = req.body.text;
+    const author = req.body.author;
+    const category = req.body.category;
+    const date = new Date();
+
+    const blogPost = {
+      title: title,
+      text: text,
+      author: author,
+      category: category,
+      createdAt: date,
+      lastModified: date,
+      id: Number(lastBlog.id + 1),
+    };
+    await collection.insertOne(blogPost);
+    res.status(200).json({ message: "Successfully Posted", success: true });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error posting blog." + error, success: false });
   }
 });
 
